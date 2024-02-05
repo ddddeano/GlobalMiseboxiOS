0607493aafd5d731f83cbfa51c0df96d949ac4ea
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-05 18:33:35 +0100

    public struct DocCollection {

--- Sources/GlobalMiseboxiOS/Global.swift
+++ Sources/GlobalMiseboxiOS/Global.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
-let defaultImage = "https://firebasestorage.googleapis.com/v0/b/misebox-78f9c.appspot.com/o/avatars%2Fdog1.jpg?alt=media&token=a8d8e14e-01af-45ae-8e9d-2033c5a81ec4"
+public let defaultImage = "https://firebasestorage.googleapis.com/v0/b/misebox-78f9c.appspot.com/o/avatars%2Fdog1.jpg?alt=media&token=a8d8e14e-01af-45ae-8e9d-2033c5a81ec4"
 
-protocol ManagerDelegate: AnyObject {
+public protocol ManagerDelegate: AnyObject {
     func resetData()
+}
+
+public struct DocCollection {
+    public var doc: String
+    public var collection: String
 }
