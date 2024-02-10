e95574daca22f0078ff8cf3ab290e7bf060856af
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-10 16:31:38 +0100

    func mixedWithBlack(fraction: CGFloat) -> Color {

--- Sources/GlobalMiseboxiOS/UXDesignComponents/Components/AvatarView.swift
+++ Sources/GlobalMiseboxiOS/UXDesignComponents/Components/AvatarView.swift
@@ -1,13 +1,4 @@
-//
-//  AvatarView.swift
-//
-//
-//  Created by Daniel Watson on 09.02.2024.
-//
-
-import Foundation
 import SwiftUI
-
 public enum Env {
     case edit
     case content(show: Bool)
@@ -42,36 +33,15 @@ public struct AvatarView: View {
     
     public var body: some View {
         ZStack {
-            circleBackground()
-            if env.shouldShow {
-                showRing()
-            }
             image()
-            bubble()
-                .frame(width: width * 0.20, height: height * 0.20)
-                .offset(x: width * 0.33, y: width * 0.33)
+            if env.shouldShow {
+                bubble()
+                    .frame(width: width * 0.20, height: height * 0.20)
+                    .offset(x: width * 0.33, y: height * 0.33)
+            }
         }
         .onTapGesture(perform: onTap)
-    }
-    
-    private func circleBackground() -> some View {
-        Circle()
-            .fill(palette.highlightGradient) // Use the highlight gradient from the palette
-            .frame(width: width, height: height)
-            .overlay(
-                Circle().stroke(Color.black, lineWidth: width * 0.01)
-            )
-    }
-
-    
-    private func showRing() -> some View {
-        Circle()
-            .stroke(lineWidth: 4)
-            .foregroundColor(.white) // Customize this as needed
-            .frame(width: width * 0.85, height: height * 0.85)
-            .overlay(
-                Circle().stroke(Color.black, lineWidth: width * 0.01)
-            )
+        .frame(width: width, height: height)
     }
     
     private func image() -> some View {
@@ -83,18 +53,16 @@ public struct AvatarView: View {
                 image
                     .resizable()
                     .scaledToFill()
-                    .frame(width: width * (env.shouldShow ? 0.9 : 1))
                     .clipShape(Circle())
                     .overlay(
-                        Circle()
-                            .stroke(Color(.black), lineWidth: width * 0.02)
+                        Circle().stroke(palette.borderColor, lineWidth: width * 0.02)
                     )
             case .failure:
                 Image(systemName: "exclamationmark.triangle")
                     .resizable()
                     .scaledToFit()
                     .foregroundColor(.red)
-                    .frame(width: width * 0.6, height: height * 0.6)
+                    .frame(width: width * 0.5, height: height * 0.5)
             @unknown default:
                 EmptyView()
             }
@@ -102,97 +70,86 @@ public struct AvatarView: View {
     }
     
     private func bubble() -> some View {
-        Group {
-            switch env {
-            case .edit:
+        switch env {
+        case .edit:
+            return AnyView(
                 Image(systemName: "pencil.circle")
                     .resizable()
                     .scaledToFit()
                     .foregroundColor(.white)
                     .background(Circle().fill(palette.primaryColor))
-            case .content(let show) where show:
-                // New Content Bubble Configuration
+                    .frame(width: width * 0.20, height: height * 0.20)
+            )
+        case .content(let show) where show:
+            return AnyView(
                 Circle()
-                    .fill(LinearGradient(gradient: Gradient(colors: [NewContentColors.color1, NewContentColors.color2]), startPoint: .topLeading, endPoint: .bottomTrailing))
-
-            case .notification(let show) where show:
+                    .fill(Color.newContent)
+                    .frame(width: width * 0.20, height: height * 0.20)
+            )
+        case .notification(let show) where show:
+            return AnyView(
                 Circle()
-                    .fill(LinearGradient(gradient: Gradient(colors: [NotificationColors.color1, NotificationColors.color2]), startPoint: .topLeading, endPoint: .bottomTrailing))
-
-            default:
-                EmptyView()
-            }
+                    .fill(Color.newNotification)
+                    .frame(width: width * 0.20, height: height * 0.20)
+            )
+        default:
+            return AnyView(EmptyView())
         }
     }
-
-
 }
 import SwiftUI
 
 struct AvatarView_Previews: PreviewProvider {
-    static let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3) // Adjust the count if you want a different layout
-
     static var previews: some View {
-        ZStack {
-            // Background using primary gradient from devPalette1
-            Rectangle()
-                .fill(devPalette1.backgroundGradient)
-                .edgesIgnoringSafeArea(.all)
+        VStack(spacing: 20) {
+            Text("Preview")
+                .foregroundColor(.white)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10).fill(userPalette.primaryColor))
             
-            // Display avatars in a grid
-            LazyVGrid(columns: columns, spacing: 20) {
-                // Edit Environment Avatar
-                AvatarView(
-                    imageUrl: defaultImage,
-                    width: 100,
-                    height: 100,
-                    env: .edit,
-                    onTap: { print("Edit Avatar tapped") },
-                    palette: devPalette1
-                )
-
-                // Content Environment Avatar (show: true)
-                AvatarView(
-                    imageUrl: defaultImage,
-                    width: 100,
-                    height: 100,
-                    env: .content(show: true),
-                    onTap: { print("Content (show: true) Avatar tapped") },
-                    palette: devPalette1
-                )
-
-                // Content Environment Avatar (show: false)
-                AvatarView(
-                    imageUrl: defaultImage,
-                    width: 100,
-                    height: 100,
-                    env: .content(show: false),
-                    onTap: { print("Content (show: false) Avatar tapped") },
-                    palette: devPalette1
-                )
-
-                // Notification Environment Avatar (show: true)
-                AvatarView(
-                    imageUrl: defaultImage,
-                    width: 100,
-                    height: 100,
-                    env: .notification(show: true),
-                    onTap: { print("Notification (show: true) Avatar tapped") },
-                    palette: devPalette1
-                )
-
-                // Notification Environment Avatar (show: false)
-                AvatarView(
-                    imageUrl: defaultImage,
-                    width: 100,
-                    height: 100,
-                    env: .notification(show: false),
-                    onTap: { print("Notification (show: false) Avatar tapped") },
-                    palette: devPalette1
-                )
-            }
-            .padding()
+            AvatarView(
+                imageUrl: defaultImage,
+                width: 100,
+                height: 100,
+                env: .edit,
+                onTap: { print("Edit Avatar tapped") },
+                palette: userPalette
+            )
+            AvatarView(
+                imageUrl: defaultImage,
+                width: 100,
+                height: 100,
+                env: .content(show: true),
+                onTap: { print("Content (show: true) Avatar tapped") },
+                palette: userPalette
+            )
+            AvatarView(
+                imageUrl: defaultImage,
+                width: 100,
+                height: 100,
+                env: .content(show: false),
+                onTap: { print("Content (show: false) Avatar tapped") },
+                palette: userPalette
+            )
+            AvatarView(
+                imageUrl: defaultImage,
+                width: 100,
+                height: 100,
+                env: .notification(show: true),
+                onTap: { print("Notification (show: true) Avatar tapped") },
+                palette: userPalette
+            )
+            AvatarView(
+                imageUrl: defaultImage,
+                width: 100,
+                height: 100,
+                env: .notification(show: false),
+                onTap: { print("Notification (show: false) Avatar tapped") },
+                palette: userPalette
+            )
         }
+        .padding()
         .previewLayout(.sizeThatFits)
+        .background(userPalette.background.edgesIgnoringSafeArea(.all))
     }
 }
--- Sources/GlobalMiseboxiOS/UXDesignComponents/Theme.swift
+++ Sources/GlobalMiseboxiOS/UXDesignComponents/Theme.swift
@@ -1,20 +1,6 @@
 import SwiftUI
-extension Color {
-    // Method to  a color
-    func lighter(by percentage: CGFloat = 30.0) -> Color {
-        let uiColor = UIColor(self)
-        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
-        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-        
-        let adjustment = 1.0 + percentage / 100.0
-        return Color(UIColor(red: min(red * adjustment, 1.0),
-                             green: min(green * adjustment, 1.0),
-                             blue: min(blue * adjustment, 1.0),
-                             alpha: alpha))
-    }
-}
 
-// MARK: - Color Definitions
+// MARK: - Food-based Color Definitions
 public extension Color {
     static let BlueberryBlue = Color(red: 0.05, green: 0.20, blue: 0.35)
     static let SkyBerryBlue = Color(red: 0.60, green: 0.80, blue: 0.90)
@@ -36,98 +22,51 @@ public extension Color {
     static let AvocadoGreen = Color(red: 0.24, green: 0.82, blue: 0.44)
     static let MatchaGreen = Color(red: 0.68, green: 0.85, blue: 0.70)
     static let CoconutMilk = Color(red: 0.97, green: 0.97, blue: 0.97)
-}
-
-// MARK: - Universal Colours
-public extension Color {
     static let PersimmonOrange = Color(red: 1.00, green: 0.44, blue: 0.37)
-}
-// MARK: - Specific Use Case Colors
-public struct NotificationColors {
-    static let color1 = Color.AvocadoGreen
-    static let color2 = Color.MatchaGreen
+
+    // System Colors
+    static var newContent: Color {
+        return .PersimmonOrange
+    }
+    
+    static var newNotification: Color {
+        return .SaffronSpice
+    }
 }
 
-public struct NewContentColors {
-    static let color1 = Color.SaffronSpice
-    static let color2 = Color.PaprikaRed
+extension Color {
+    func mixedWithBlack(fraction: CGFloat) -> Color {
+        guard let rgbColor = UIColor(self).cgColor.components, rgbColor.count >= 3 else {
+            return self // Return the original color if conversion fails
+        }
+
+        // Calculate the new color components
+        let red = rgbColor[0] * (1 - fraction)
+        let green = rgbColor[1] * (1 - fraction)
+        let blue = rgbColor[2] * (1 - fraction)
+
+        return Color(red: Double(red), green: Double(green), blue: Double(blue))
+    }
 }
-import SwiftUI
 
 public struct Palette {
     public var primaryColor: Color
-    public var primaryColorLight: Color { primaryColor.lighter() }
-    
-    // Additional color properties
-    public var color3: Color
-    public var color4: Color
-    public var color5: Color
-    public var color6: Color
-    public var color7: Color
-    public var color8: Color
 
-    // Updated initializer to include all colors
-    public init(primaryColor: Color, color3: Color, color4: Color, color5: Color, color6: Color, color7: Color, color8: Color) {
+    public init(primaryColor: Color) {
         self.primaryColor = primaryColor
-        self.color3 = color3
-        self.color4 = color4
-        self.color5 = color5
-        self.color6 = color6
-        self.color7 = color7
-        self.color8 = color8
     }
-    
-    public var backgroundGradient: LinearGradient {
-        LinearGradient(gradient: Gradient(colors: [primaryColor, primaryColorLight]), startPoint: .topLeading, endPoint: .bottomTrailing)
-    }
-    
-    public var highlightGradient: LinearGradient {
-          LinearGradient(
-              gradient: Gradient(colors: [primaryColor, .PersimmonOrange, primaryColor, primaryColor]), // Three times primary color, then orange
-              startPoint: .topLeading,
-              endPoint: .bottomTrailing
-          )
-      }
 
+    public var background: Color {
+        return primaryColor
+    }
+
+    public var borderColor: Color {
+        return primaryColor.mixedWithBlack(fraction: 0.8) 
+    }
 }
 
-// Updated palette instances
-public let userPalette1 = Palette(
-    primaryColor: .KaleGreen,
-    color3: .MatchaGreen,
-    color4: .KaleGreen,
-    color5: .LavenderHoney,
-    color6: .VanillaCream,
-    color7: .TomatoRed,
-    color8: .WatermelonPink
-)
 
-public let devPalette1 = Palette(
-    primaryColor: .KaleGreen,
-    color3: .MatchaGreen,
-    color4: .KaleGreen,
-    color5: .LavenderHoney,
-    color6: .VanillaCream,
-    color7: .TomatoRed,
-    color8: .WatermelonPink
-)
 
-public let devPalette2 = Palette(
-    primaryColor: .LemonZest,
-    color3: .RaspberryMousse,
-    color4: .MintLeaf,
-    color5: .StrawberryFrost,
-    color6: .AvocadoGreen,
-    color7: .MangoTango,
-    color8: .GrapeSoda
-)
 
-public let devPalette3 = Palette(
-    primaryColor: .PersimmonOrange,
-    color3: .BlueberryBlue,
-    color4: .SkyBerryBlue,
-    color5: .MatchaGreen,
-    color6: .KaleGreen,
-    color7: .LavenderHoney,
-    color8: .VanillaCream
-)
+// Example usage
+public let userPalette = Palette(primaryColor: .BlueCuracao)
