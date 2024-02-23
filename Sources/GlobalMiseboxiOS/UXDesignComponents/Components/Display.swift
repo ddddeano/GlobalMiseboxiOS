7ab0f481b52b0313b408552a8888427445d1f6ed
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-23 12:44:16 +0100

    public struct SectionTitle: View {

--- Sources/GlobalMiseboxiOS/UXDesignComponents/Components/Display.swift
+++ Sources/GlobalMiseboxiOS/UXDesignComponents/Components/Display.swift
@@ -1,8 +1,20 @@
 //
-//  File.swift
+//  Display.swift
 //  
 //
 //  Created by Daniel Watson on 23.02.2024.
 //
 
 import Foundation
+import SwiftUI
+
+public struct SectionTitle: View {
+    var title: String
+    public var body: some View {
+        Text(title.uppercased())
+            .font(.caption)
+            .fontWeight(.bold)
+            .padding(.top, 2)
+            .foregroundColor(.primary)
+    }
+}
