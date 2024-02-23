2ec9bb00d48dd6fe1fbf985eb043fdd5fa633bcb
Daniel Watson <d.m.watson1985@gmail.com>
2024-02-23 12:47:12 +0100

    public struct EditToggleImageButton: View {

--- Sources/GlobalMiseboxiOS/UXDesignComponents/Components/Buttons.swift
+++ Sources/GlobalMiseboxiOS/UXDesignComponents/Components/Buttons.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import SwiftUI
+
+public struct EditToggleImageButton: View {
+    @Binding var isEditing: Bool
+    var isValid: Bool
+    let onEdit: () -> Void
+    let onDone: () async -> Void
+    let onCancel: () -> Void
+
+    public var body: some View {
+        HStack {
+            if isEditing {
+                Image(systemName: "x.circle")
+                    .foregroundColor(.red)
+                    .onTapGesture {
+                        onCancel()
+                        isEditing = false
+                    }
+            }
+            
+            Image(systemName: isEditing ? "checkmark.circle" : "pencil.circle")
+                .foregroundColor(.blue)
+                .onTapGesture {
+                    if isEditing {
+                        Task {
+                            await onDone()
+                            isEditing = false
+                        }
+                    } else if !isEditing {
+                        isEditing = true
+                        onEdit()
+                    }
+                }
+        }
+    }
+}
