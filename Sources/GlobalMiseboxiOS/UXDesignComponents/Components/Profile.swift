07e1bf7a1117b7d3ca1188a7cd9afb1895f86bc7
Daniel Watson <d.m.watson1985@gmail.com>
2024-03-05 12:04:05 +0100

    public profile protocol

--- Sources/GlobalMiseboxiOS/UXDesignComponents/Components/Profile.swift
+++ Sources/GlobalMiseboxiOS/UXDesignComponents/Components/Profile.swift
@@ -1,18 +1,62 @@
+
+//  ProfileView.swift
 //
-//  SwiftUIView.swift
-//  
 //
 //  Created by Daniel Watson on 05.03.2024.
 //
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+public protocol ProfileSection: CaseIterable, Identifiable, Hashable, RawRepresentable where RawValue == String {
+    var iconName: String { get }
+    var displayName: String { get }
 }
 
-#Preview {
-    SwiftUIView()
+public struct ProfileView<Section: ProfileSection, Destination: View>: View {
+    @EnvironmentObject var env: EnvironmentManager
+    let sections: [Section]
+    let destinationView: (Section) -> Destination
+    @State private var navigationPath = NavigationPath()
+
+    public init(sections: [Section], destinationView: @escaping (Section) -> Destination) {
+        self.sections = sections
+        self.destinationView = destinationView
+    }
+
+    public var body: some View {
+        NavigationStack(path: $navigationPath) {
+            ScrollView {
+                ForEach(sections, id: \.self) { section in
+                    Button {
+                        navigationPath.append(section)
+                    } label: {
+                        HStack {
+                            iconView(systemName: section.iconName)
+                            Text(section.displayName)
+                            Spacer()
+                            iconView(systemName: "chevron.right")
+                        }
+                        .foregroundColor(env.appLight)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .contentShape(Rectangle())
+                    .navigationDestination(for: Section.self) { section in
+                        destinationView(section)
+                    }
+                }
+                .sectionStyle(borderColor: env.appLight)
+            }
+            .padding()
+        }
+        .pageStyle(backgroundColor: env.appDark.opacity(0.1))
+    }
+
+    @ViewBuilder
+    private func iconView(systemName: String) -> some View {
+        Image(systemName: systemName)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 24, height: 24)
+    }
 }
